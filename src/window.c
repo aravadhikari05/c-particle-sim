a36f4aa7a0f69915d0e3c098b885a7689ab64a3c
arav <aravadhikari05@gmail.com>
2024-06-25 20:04:07 -0700

    added position change

--- src/window.c
+++ src/window.c
@@ -12,20 +12,15 @@
 
 char* vertex_shader_source = "#version 410 core\n"
   "layout (location = 0) in vec3 aPos;\n"
+  "uniform float x_offset;\n"
   "void main() {\n"
-  "  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+  "  gl_Position = vec4(aPos.x + x_offset, aPos.y, aPos.z, 1.0);\n"
   "}\0";
 
-char* fragment_shader_source = "#version 330 core\n"
+char* fragment_shader_source = "#version 410 core\n"
   "out vec4 FragColor;\n"
   "void main() {\n"
   "  FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
-  "}\0"; 
-
-char* fragment_shader_source2 = "#version 330 core\n"
-  "out vec4 FragColor;\n"
-  "void main() {\n"
-  "  FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
   "}\0"; 
 
 void error_callback(int error, const char* description) {
@@ -52,7 +47,7 @@ int main(void) {
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   
-  GLFWwindow* window = glfwCreateWindow(WINDOW_LENGTH, WINDOW_HEIGHT, "Phoenix", NULL, NULL);
+  GLFWwindow* window = glfwCreateWindow(WINDOW_LENGTH, WINDOW_HEIGHT, "phoenix", NULL, NULL);
   if (!window) {
     printf("Window could not be created\n");
     glfwTerminate();
@@ -65,56 +60,29 @@ int main(void) {
 
   glfwSwapInterval(1);
 
-  GLuint shader_program = create_shader_program(vertex_shader_source, fragment_shader_source); 
-  GLuint shader_program2 = create_shader_program(vertex_shader_source, fragment_shader_source2); 
+  GLuint shader_program = shader_program_create(vertex_shader_source, fragment_shader_source); 
 
   const float vertices[] = {
     -0.5f, -0.5f, 0.0f,
-     0.0f, -0.5f, 0.0f,
-     -0.25f,  0.0f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f,
   }; 
 
-  const float vertices2[] = {
-    0.5f, -0.5f, 0.0f,
-     0.0f, -0.5f, 0.0f,
-     0.25f,  0.0f, 0.0f
-  };
+  unsigned int VBO, VAO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
 
-  const float sq_vertices[] = {
-       0.0f,  0.0f, 0.0f,  // top right
-       0.0f, -0.5f, 0.0f,  // bottom right
-      -0.5f, -0.5f, 0.0f,  // bottom left
-      -0.5f,  0.0f, 0.0f   // top left
-  };
+  glBindVertexArray(VAO);
 
-  unsigned int sq_indices[] = {
-    0, 1, 3,
-    1, 2, 3
-  };
-
-  unsigned int VBOs[2], VAOs[2], EBO;
-  glGenVertexArrays(2, VAOs);
-  glGenBuffers(2, VBOs);
-  glGenBuffers(1, &EBO);
-
-  glBindVertexArray(VAOs[0]);
-
-  glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(sq_vertices), sq_vertices, GL_STATIC_DRAW); 
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(sq_indices), sq_indices, GL_STATIC_DRAW);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);  
  
-  glBindVertexArray(VAOs[1]);
+  glBindVertexArray(VAO);
   
-  glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW); 
-  
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-  glEnableVertexAttribArray(0);   
-
+  float x_offset = 0.0f;
   while (!glfwWindowShouldClose(window)) {
     processInput(window);
 
@@ -122,11 +90,12 @@ int main(void) {
     glClear(GL_COLOR_BUFFER_BIT);
 
     glUseProgram(shader_program);
-    glBindVertexArray(VAOs[0]);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    GLint x_offset_location = glGetUniformLocation(shader_program, "x_offset");
+    glUniform1f(x_offset_location, x_offset);
 
-    glUseProgram(shader_program2);
-    glBindVertexArray(VAOs[1]);
+    x_offset += 0.001f;
+
+    glBindVertexArray(VAO);
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
     glfwSwapBuffers(window);
